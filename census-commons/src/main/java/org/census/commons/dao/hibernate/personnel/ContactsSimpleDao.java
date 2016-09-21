42c827942abe6198c000ddd50687cd6ed632d510
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2016-09-21 23:22:18 +0300

    Removed unnecessary methods.

--- census-commons/src/main/java/org/census/commons/dao/hibernate/personnel/ContactsSimpleDao.java
+++ census-commons/src/main/java/org/census/commons/dao/hibernate/personnel/ContactsSimpleDao.java
@@ -1,14 +1,12 @@
 package org.census.commons.dao.hibernate.personnel;
 
-import org.apache.commons.lang3.StringUtils;
 import org.census.commons.dao.AbstractHibernateDao;
-import org.census.commons.dto.ContactType;
 import org.census.commons.dto.personnel.ContactDto;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Dao component for working with Contact domain object.
+ * Dao component for working with Contact domain entity object.
  * @author Gusev Dmitry (gde)
  * @version 1.0 (DATE: 04.08.2015)
  */
@@ -19,30 +17,6 @@ public class ContactsSimpleDao extends AbstractHibernateDao<ContactDto> {
 
     public ContactsSimpleDao() {
         super(ContactDto.class);
-    }
-
-    /***/
-    // todo: add contact type to checking?
-    public ContactDto getContactByContent(String content) {
-        return (ContactDto) this.getSessionFactory().getCurrentSession().createQuery(
-                "select c from ContactDto as c where c.contact = '" + content + "'"
-        ).uniqueResult();
-    }
-
-    /***/
-    public ContactDto addContactByContent(String content, ContactType type) {
-
-        if (StringUtils.isBlank(content)) {
-            throw new IllegalArgumentException("Can't add contact by null content!");
-        }
-
-        ContactDto contact = this.getContactByContent(content);
-        if (contact == null) {
-            contact = new ContactDto(0, content, null, type);
-            this.save(contact);
-        }
-
-        return contact;
     }
 
 }
