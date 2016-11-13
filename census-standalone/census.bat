e682866c4ff8f18e937b648372e6413f382737b1
Dmitry Gusev <dmitry.ed.gusev@gmail.com>
2016-11-13 23:05:45 +0300

    Added starting batch, added build number generation, minor POMs changes. Dev step.

---
+++ census-standalone/census.bat
@@ -0,0 +1,11 @@
+@echo off
+rem *****************************************************************
+rem *                                                               *
+rem *   Batch file for starting Census standalone application.      *
+rem *                                                               *
+rem *   Created: Dmitry Gusev, 11.2016                              *
+rem *   Modified:                                                   *
+rem *****************************************************************
+
+echo --- Starting Census standalone application ---
+java -jar @JAR_NAME@.jar
