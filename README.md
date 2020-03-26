03de87b1bcced1fa22c15b5a8be6cbc30baec39a
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2020-03-26 23:12:57 +0100

    Improved doc. Minor fixes for POM files.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Census simple ERP system, (C) 2010-2020 #
+
+## How to build project ##
+TBD
+
+## Project modules ##
+* **[census-auth]** Simple authorization microservice. Works via REST over http/https protocol.
+* **[census-service1]**
+* **[census-service2]**
+* **[census-service3]**
+
+## Detailed modules descriptions ##
+
+### [census-auth] ###
+Simple authorization microservice.
+Base service URI: **host:port/census/auth/xxx**
+
+#### REST interface description ####
+| HTTP method |  URI      | Description |
+| ----------- | --------- | ----------- |
+| GET         |   /users  | get list of all users |
+| POST        |   /users  | save new user |
+| GET         |   /roles  | get list of all roles |
+| POST        |   /roles  | save new role |
+| -         |   -   | - |
+| -         |   -   | - |
+| -         |   -   | - |
+| -         |   -   | - |
+| -         |   -   | - |
+
+### [census-service1] ###
+TBD
+
+### [census-service2] ###
+TBD
+
+### [census-service3] ###
+TBD
--- census-auth/src/main/java/org/dgusev/census/auth/api/AuthRestController.java
+++ census-auth/src/main/java/org/dgusev/census/auth/api/AuthRestController.java
@@ -4,10 +4,8 @@ import org.dgusev.census.auth.domain.AuthRole;
 import org.dgusev.census.auth.domain.AuthUser;
 import org.dgusev.census.auth.repository.AuthRoleRepository;
 import org.dgusev.census.auth.repository.AuthUserRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /***/
 
@@ -24,14 +22,30 @@ public class AuthRestController {
         this.authRoleRepository = authRoleRepository;
     }
 
+    // Find users with assigned roles
     @GetMapping("/users")
     public Iterable<AuthUser> listUsers() {
         return authUserRepository.findAll();
     }
 
+    // Save user; return 201 instead of 200
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/users")
+    AuthUser newAuthUser(@RequestBody AuthUser newAuthUser) {
+        return authUserRepository.save(newAuthUser);
+    }
+
+    // Find roles
     @GetMapping("/roles")
     public Iterable<AuthRole> listRoles() {
         return authRoleRepository.findAll();
+    }
+
+    // Save role; return 201 instead of 200
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/roles")
+    AuthRole newAuthRole(@RequestBody AuthRole newAuthRole) {
+        return authRoleRepository.save(newAuthRole);
     }
 
 }
