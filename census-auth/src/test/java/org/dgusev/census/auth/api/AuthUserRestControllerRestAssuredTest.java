c68e4938a31a7ea80d705a7b035aae06f1dbe0c9
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2020-04-21 20:12:38 +0200

    Added initial REST-Assured test (+POM dependency).

--- census-auth/src/test/java/org/dgusev/census/auth/api/AuthUserRestControllerRestAssuredTest.java
+++ census-auth/src/test/java/org/dgusev/census/auth/api/AuthUserRestControllerRestAssuredTest.java
@@ -1,6 +1,30 @@
 package org.dgusev.census.auth.api;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.equalTo;
+
 /***/
 
+// todo: https://www.baeldung.com/rest-assured-tutorial
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AuthUserRestControllerRestAssuredTest {
+
+    @Test
+    public void test() {
+
+        get("/census/api/auth/users/1")
+                .then()
+                    .statusCode(200)
+                    .assertThat()
+                    .body("id", equalTo(1));
+
+    }
+
 }
